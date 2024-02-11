3632bc1165c076baca9807122ea3e9fab8bc16e7
Fitiavana Anhy Krishna <fitiavana.krishna@gmail.com>
2024-02-11 16:55:23 +0300

    Update utils.py

--- googlesearchpython/utils.py
+++ googlesearchpython/utils.py
@@ -1,14 +1,15 @@
 import re
 
 def extract_html_block(text):
-    # regex pattern
-    pattern = r'(<div data-state-token="[^"]*" decode-data-ved="1"[^>]*>.*?3;\[9\]0;)'
-    
-    # find matches
-    matches = re.findall(pattern, text, re.DOTALL)
+    patterns = [
+        r'(<div data-state-token="[^"]*" decode-data-ved="1"[^>]*>.*?3;\[9\]0;)',
+        r'(<div decode-data-ved="1"[^>]*>.*?3;\[9\]0;)',
+    ]
 
-    # process matches
-    for match in matches:
-        # remove ending string
-        cleaned_match = match.replace('3;[9]0;', '')
-        return cleaned_match
+    for pattern in patterns:
+        matches = re.findall(pattern, text, re.DOTALL)
+
+        for match in matches:
+            # remove ending string
+            cleaned_match = match.replace('3;[9]0;', '')
+            return cleaned_match
