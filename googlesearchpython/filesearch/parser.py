f42c38b364e46ac1c5f1a12f7eb567f71da312c5
Fitiavana Anhy Krishna <fitiavana.krishna@gmail.com>
2024-02-11 10:55:32 +0300

    Update parser, bump to version 2024.02.11-beta

--- googlesearchpython/filesearch/parser.py
+++ googlesearchpython/filesearch/parser.py
@@ -13,12 +13,13 @@ def parse_html_result(html_result):
         results_blocks = results_blocks.findAll("div", class_="N54PNb BToiNc cvP2Ce")
         if len(results_blocks) != 0:
             for result_block in results_blocks:
+                # logger.debug(f"{result_block}\n----------------------------")
                 try:
                     result = {
                         "title": result_block.find("h3").text,
                         "url": result_block.findAll("a")[0]["href"],
                         "description": unicodedata.normalize(
-                            "NFKD", result_block.find("div", class_="VwiC3b yXK7lf lVm3ye r025kc hJNv6b Hdw6tb").text)
+                            "NFKD", result_block.find("div", class_="VwiC3b yXK7lf fS1kJf lVm3ye r025kc hJNv6b Hdw6tb").text)
                     }
                 except AttributeError:
                     logger.error(traceback.format_exc())
